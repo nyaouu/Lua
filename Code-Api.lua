ea1515a7899dfbf8ec61cbbeb05d87cf9e17c197
nyaouu <105587184+nyaouu@users.noreply.github.com>
2022-11-13 14:38:37 +0700

    Update Code-Api.lua

--- Code-Api.lua
+++ Code-Api.lua
@@ -1,5 +1,5 @@
   _G.Code = {
-      King_Lagecy = {
+      King_Legacy = {
           Gem = {"UPDATE4.0.2", "HYDRAGLYPHICS", "UPDATE4", "THXFOR1BVISIT"},
           Cash = {"Peodiz", "DinoxLive"},
           ReStats = {"900KLIKES"}
