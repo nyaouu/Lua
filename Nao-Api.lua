758727bd49bfe20dc4c34b6c07142112097c0c52
nyaouu <105587184+nyaouu@users.noreply.github.com>
2022-11-17 20:18:40 +0700

    Update Nao-Api.lua

--- Nao-Api.lua
+++ Nao-Api.lua
@@ -3,5 +3,12 @@
           Gem = {"UPDATE4.0.2", "HYDRAGLYPHICS", "UPDATE4", "THXFOR1BVISIT"},
           Cash = {"Peodiz", "DinoxLive"},
           ReStats = {"900KLIKES"}
+      },
+      Blox_Fruits = {
+          X2EXP = {},
+          Restats = {}
       }
   }
+
+_G.Blox_Fruit_PlaceId_List = { 2753915549 , 4442272183 , 7449423635 }
+_G.King_Legacy_PlaceId_List = { 4520749081 , 6381829480 , 5931540094 }
